254c7385caee07664b82349336ab40e32b02f9ce
Luca Gronau Manne <luca.manne@csj.edu.pt>
2023-10-09 15:33:40 +0100

    add age checker exercise

---
+++ ageChecker.js
@@ -0,0 +1,10 @@
+// 1. Define a constant `age` with a numerical value representing a person's age. For
+// example, you can set `const age = 18;` to represent a person who is 18 years old.
+// 2. Use a ternary operator to check the person's age and assign a message to a variable
+// `message`. If the person is 18 or older, the message should say "You are eligible to
+// vote." Otherwise, the message should say "You are not eligible to vote."
+// 3. Display the `message` to the user.
+const age = 67
+let eligibility = 
+(age >17) ? "You are eligible to vote!" : "You are not eligible to vote"
+console.log(eligibility);
